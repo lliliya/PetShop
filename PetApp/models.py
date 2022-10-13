86ca0535a755908bb184fdd9ec4d63247c52f9cd
lliliya <lilya-2004@mail.ru>
2022-10-13 19:45:51 +0300

    Project creation

---
+++ PetApp/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class SiteUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True, default='NoName')
+
+
+class Goods(models.Model):
+    vendor_code = models.IntegerField(default=0)
+    name = models.CharField(max_length=50, default='NoName')
+    price = models.FloatField(default=0.00)
+    weight = models.FloatField(default=0.00)
+    description = models.TextField(default='Description not specified')
+    composition = models.TextField(default='Сomposition not specified')
+    category = models.ManyToManyField(Category, through='GoodsCategory')
+    product_rating = models.IntegerField(default=0)
+
+    def like(self, amount=0):
+        self.product_rating += amount
+        self.save()
+
+    def dislike(self):
+        self.like(-1)
+
+
+class Seller(models.Model):
+    name = models.CharField(max_length=50, unique=True, default='NoName')
+    seller_rating = models.FloatField(default=0.0)
+    contacts = models.TextField(default='The seller did not specify his contacts')
+
+    def update_rating(self):
+        self.seller_rating = 0
+        for order in Order.objects.filter(seller_id=self.name):
+            self.seller_rating += order.order_rating
+        self.save()
+
+
+class Review(models.Model):
+    datetime_creation = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(default='No Text')
+
+
+class Order(models.Model):
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default=0.00)
+    pickup = models.BooleanField(default=False)  # if pickup, then True, if delivery, then False
+    complete = models.BooleanField(default=False)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    goods = models.ManyToManyField(Goods, through='GoodsOrder')
+    order_rating = models.IntegerField(default=0)
+
+    def like(self, amount=0):
+        self.order_rating += amount
+        self.save()
+
+    def dislike(self):
+        self.like(-1)
+
+
+class GoodsOrder(models.Model):
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
+
+
+class GoodsCategory(models.Model):
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
